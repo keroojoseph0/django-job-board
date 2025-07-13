4c85a0fc9592db231e6f26bcb68d492c06a5c7d3
keroojoseph0 <keroojoseph0@gmail.com>
2025-07-13 19:02:41 +0300

    ADD job views and urls

--- job/admin.py
+++ job/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from job.models import Job
+from job.models import Job, Category
+
 # Register your models here.
 
 admin.site.register(Job)
+admin.site.register(Category)
