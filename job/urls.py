cf5080796efe5c8cb512899a427ef547836b140c
keroojoseph0 <keroojoseph0@gmail.com>
2025-07-30 14:23:01 +0300

    Add REST api

--- job/urls.py
+++ job/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from . import api
 
 app_name = 'job'
 
@@ -7,4 +8,7 @@ urlpatterns = [
     path('', views.job_list, name = 'job_list'),
     path('add', views.add_job, name = 'add_job'),
     path('<str:slug>', views.job_detail, name = 'job_detail'),
+
+    # api
+    path('api/list', api.joblistapi, name = 'joblistapi'),
 ]
--- project/settings.py
+++ project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django_bootstrap5',
     'django_filters',
     'contact',
+    'rest_framework'
     
 
 ]
