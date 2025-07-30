13cd9bda9ee583550c17e5fae123dd2cf4d57276
keroojoseph0 <keroojoseph0@gmail.com>
2025-07-30 13:34:28 +0300

    Add contact email message

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'job.apps.JobConfig',
     'django_bootstrap5',
     'django_filters',
+    'contact',
     
 
 ]
@@ -136,3 +137,13 @@ MEDIA_ROOT =  BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'keroojoseph2@gmail.com'
+EMAIL_HOST_PASSWORD = 'afmp ztvt gkus jtbm'
+EMAIL_USE_TLS = True
+
